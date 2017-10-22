7bb66e1d4458498aac5e24c2c586275a71fe1e6b
MUHAMMED\muhammed <m.recep.caylak@gmail.com>
2017-10-22 03:02:46 +0300

    Web sitesi templateti visual studioya giydirildi. Ayrıca admin template hazırlandı ve visual studioya giydirildi .Projeyi açmadan önce benioku.txt ye bakmayı unutma

---
+++ web-proje/WebApp2/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult AnaSayfa()
+        {
+            return View();
+        }
+        public ActionResult Etkinlikler()
+        {
+            return View();
+        }
+        public ActionResult DersNotu()
+        {
+            return View();
+        }
+        public ActionResult Esya()
+        {
+            return View();
+        }
+        public ActionResult Tanitim()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult BizeUlasın()
+        {
+            return View();
+        }
+
+
+    }
+}
