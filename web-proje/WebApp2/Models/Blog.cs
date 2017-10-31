b4deee99f88c20364c00e9e4ab9f625558d163ab
MUHAMMED\muhammed <m.recep.caylak@gmail.com>
2017-10-31 23:24:08 +0300

    Veri Tabani için classlar olusturuldu. Anasayfa.cshtml dosyası üzerinde değişikliklere gidildi.

---
+++ web-proje/WebApp2/Models/Blog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp2.Models
+{
+    public class Blog
+    {
+        public int BlokID { get; set; }
+        public int YazarID { get; set;}
+        public string BlogResimYol { get; set; }
+        public string BlogIcerik { get; set; }
+        public string BlogBaslıgı { get; set; }
+    }
+}
---
+++ web-proje/WebApp2/Models/Kullanici.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp2.Models
+{
+    public class Kullanici
+    {
+        public int KullaniciID { get; set; }
+        public string KullaniciAdi { get; set; }
+        public string KullaniciSoyadi { get; set; }
+        public string KullaniciMail { get; set; }
+        public int KullaniciSifre { get; set; }
+        public DateTime KullaniciKayitTarihi{ get; set; }
+        public bool KullaniciTipi { get; set; }
+        public virtual ICollection<Urunler> Urun { get; set; }
+        public virtual ICollection<DersNotu> Notlar { get; set; }
+    }
+}
